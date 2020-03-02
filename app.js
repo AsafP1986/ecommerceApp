26f03bb1c2e69f6c36349ffd1a516e41dc9a147c
P.Asaf <pasaf1@gmail.com>
2020-03-02 19:12:23 +0200

    dsfsd

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ app.use("/users", usersRouter);
 if (process.env.NODE_ENV === "production") {
   // app.use(express.static("client/dist/"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
+    res.sendFile(path.resolve("client", "dist", "client", "index.html"));
   });
 }
 
