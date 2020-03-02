7a50961684b09c6987c0f3785322560e06eeb8df
P.Asaf <pasaf1@gmail.com>
2020-03-02 16:48:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -43,7 +43,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/dist/client"));
   app.get("*", (req, res) => {
     res.sendFile(
-      path.resolve(__dirname, "client", "dist/client", "index.html")
+      path.resolve(__dirname, "client", "dist", "client", "index.html")
     );
   });
 }
