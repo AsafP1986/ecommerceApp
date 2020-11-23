ac9c1fd3787661dfc54c3bfd2f72ea7752dd5691
P.Asaf <pasaf1@gmail.com>
2020-11-23 23:20:16 +0200

    make it bette8r

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "client/dist/")));
   app.get("*", (req, res) => {
     console.log('__dirname2', __dirname)
-    res.sendFile(path.resolve(__dirname, "client/dist/index.html"));
+    res.sendFile(path.resolve( "client/dist/index.html"));
   });
 }
 // "client",
