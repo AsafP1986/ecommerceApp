57cc4a5fc7e7fd298008774ddc8691786901c708
P.Asaf <pasaf1@gmail.com>
2020-11-24 09:32:26 +0200

    make it bette14r

--- app.js
+++ app.js
@@ -46,8 +46,7 @@ if (process.env.NODE_ENV === "production") {
   console.log('path.join(__dirname, "client/dist/")', path.join(__dirname, "client/dist/"))
   app.get("*", (req, res) => {
     console.log('__dirname2', __dirname)
-    console.log('req', req)
-    console.log('res', res)
+   
     res.sendFile(path.join( __dirname, "client/dist/client/index.html"));
   });
 }
